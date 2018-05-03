c0c4d64ff84ab185f3b09e8e729721a3aa90c263
Nick Tallant <ndtallant@uchicago.edu>
2018-05-03 11:02:11 -0500

    adding client credential framework

--- db.py
+++ db.py
@@ -14,6 +14,22 @@ class Elephant:
     '''
     psql client
     '''
+    def __init__(self, dbname, dbhost, dbport, dbusername, dbpasswd, override=False):
+    # you add class variables here like
+    # self.myvar="the greatest variable ever. the best"
+    self.dbname = dbname
+    self.dbhost = dbhost
+    self.dbport = dbport
+    self.dbusername = dbusername 
+    self.dbpasswd = dbpasswd 
 
-    def __init__():
-        pass
+    #for grading do not modify
+    if override:
+        logger.info("Overriding DB connection params")
+        self.dbname=DBVars.dbname
+        self.dbhost=DBVars.dbhost
+        self.dbport=DBVars.dbport
+        self.dbusername=DBVars.dbusername
+        self.dbpasswd=DBVars.dbpasswd
+
+    pass
