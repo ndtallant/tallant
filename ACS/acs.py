d3f0bf5e7cbb105516281ae1e3df5c67cc0deb91
Nick Tallant <ndtallant@uchicago.edu>
2018-07-27 11:37:54 -0500

    Updated query structure for new API changes

--- ACS/acs.py
+++ ACS/acs.py
@@ -1,15 +1,24 @@
 # American Community Survey
+import requests
+import pandas as pd
 
 class ACS:
     def __init__(self, year, dataset, codes, geography, key):
-        self.base = 'https://api.census.gov/data/{}/{}?get=NAME,{}&for={}&key={}'
+        self.base = 'https://api.census.gov/data/{}/{}/profile?get=NAME,{}&for={}&key={}'
         self.year = year
         self.dataset = dataset
         self.codes = ','.join(codes)
-        self.geography = geography # lookup mulitple specific geos (not just *)
+        # lookup mulitple specific geos (not just *)
+        self.geography = geography
         self.key = key
-        self.query = self.base.format(year, dataset, self.codes, geography, key) 
+        self.query = self.base.format(year, dataset
+                                    , self.codes
+                                    , geography
+                                    , key) 
 
     def see_query(self):
         '''Previews ACS query to the screen.'''
-        print(self.query[: -len(self.key)])
+        print(self.query.split('key')[0])
+
+    def get_data(self):
+        pass
