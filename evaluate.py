5158c2e064b85595ef0302fdcd1823b188404fc3
Nick Tallant <ndtallant@uchicago.edu>
2018-07-30 10:04:22 -0500

    Added confusion matrix plot

--- evaluate.py
+++ evaluate.py
@@ -4,23 +4,11 @@ tallant.evaluate
 
 This file contains functions for Machine Learning Model Evaluation 
 '''
-
+import itertools
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-# Machine Learning Methods 
-from sklearn.svm import SVC
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import LogisticRegression 
-
-from sklearn.ensemble import BaggingClassifier 
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier 
 
 # Evaluation Metrics
 from sklearn.metrics import confusion_matrix
@@ -118,3 +106,39 @@ def plot_precision_recall_n(y_true, y_score, model_name, save=True):
         plt.savefig(outfile)
     else:
         plt.show()
+
+def plot_confusion_matrix(y_true, y_pred,
+                          classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+
+    Inputs
+    ------
+            cm: confusion matrix
+            classes: class names
+    """
+    cm = confusion_matrix(y_true, y_pred, labels=classes)
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
