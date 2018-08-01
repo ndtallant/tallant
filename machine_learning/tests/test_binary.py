c18cdce6ad2e84d8ac11b4736d7d10c3b5635018
Nick Tallant <ndtallant@uchicago.edu>
2018-07-31 21:59:17 -0500

    Restructure for new pipelines

--- machine_learning/tests/test_binary.py
+++ machine_learning/tests/test_binary.py
@@ -1,9 +1,8 @@
-from pipeline import single_split_loop 
-from sklearn.datasets import load_iris
 from sklearn.datasets import load_breast_cancer
 from sklearn.cross_validation import train_test_split
-data = load_breast_cancer()
+from pipeline.binary_pipeline import single_split_loop 
 
+data = load_breast_cancer()
 X, y = data.data, data.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
