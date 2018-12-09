fe4867c38f0511e085e2a519914e240980e43121
Nick Tallant <ndtallant@uchicago.edu>
2018-12-09 15:10:20 -0600

    setup.py works, tested install

--- setup.py
+++ setup.py
@@ -4,10 +4,4 @@ setup(
     name='Tallant'
     , version='0.1dev'
     , packages=find_packages()
-    #'machine_learning'
-    #        , 'policy'
-    #        , 'stats'
-    #        , 'utils']
-    #, license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    #long_description=open('README.txt').read(),
 )
