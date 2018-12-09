1e67b24349096b331dde25a8a4f0398c691abe7b
Nick Tallant <ndtallant@uchicago.edu>
2018-12-09 15:01:39 -0600

    setup typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='Tallant'
     , version='0.1dev'
-    , packages=['tallant'
+    , packages=['tallant']
     #'machine_learning'
     #        , 'policy'
     #        , 'stats'
