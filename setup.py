4bcafe1220a17a2ae53e97f02bd8abd4ac3957a4
Nick Tallant <ndtallant@uchicago.edu>
2018-12-09 15:00:55 -0600

    Restructuring for packaging

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from distutils.core import setup
 setup(
     name='Tallant'
     , version='0.1dev'
-    , packages=['machine_learning'
-            , 'policy'
-            , 'stats'
-            , 'utils']
+    , packages=['tallant'
+    #'machine_learning'
+    #        , 'policy'
+    #        , 'stats'
+    #        , 'utils']
     #, license='Creative Commons Attribution-Noncommercial-Share Alike license',
     #long_description=open('README.txt').read(),
 )
