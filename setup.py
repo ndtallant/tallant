2074de00ba37ad51e32ddb5d8499370b927f313e
Nick Tallant <ndtallant@uchicago.edu>
2018-12-09 14:55:25 -0600

    setup try 3

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from distutils.core import setup
 setup(
     name='Tallant'
     , version='0.1dev'
-    , packages=['tallant'
-            ,]
+    , packages=['machine_learning'
+            , 'policy'
+            , 'stats'
+            , 'utils']
     #, license='Creative Commons Attribution-Noncommercial-Share Alike license',
     #long_description=open('README.txt').read(),
 )
