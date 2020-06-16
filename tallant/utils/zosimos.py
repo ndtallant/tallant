0485006cdf67b7d377ba3fc5ef73a5ec42861732
Nicholas Tallant <ndtallant@gmail.com>
2020-06-16 09:58:43 -0400

    Need to test new db wrapper

---
+++ tallant/utils/zosimos.py
@@ -0,0 +1,28 @@
+'''
+Zosimos was active around 300 AD and thought to be the first alchemist.
+He was born in the Upper Egyptian city of Panopolis, now called Akhmim.
+Zosimos allegedly wrote twenty-eight books about alchemy.
+Most of what he wrote is now lost.
+
+Anyway, this is my wrapper over sqlalchemy because I never remember
+what to do and end up googling everything.
+'''
+
+import sqlalchemy
+import pandas as pd
+
+class Alchemist:
+    '''A wrapper around sqlalchemy.'''
+
+    def __init__(connection_string):
+        '''Takes in a connection string and sets up basic things you need.'''
+        self.engine = sqlalchemy.create_engine(connection_string)        
+        self.schemas = sqlalchemy.inspect(self.engine).get_schema_names()
+        self.tables = {s: self.engine.table_names(s) for s in self.schemas}
+
+    def query(self, query, records=False):
+        '''Returns query data as a pandas DataFrame by default. For regular
+        SQLAlchemy output, use records=True.'''
+        if records:
+            return self.engine.execute(query).fetchall()
+        return pd.read_sql(query, self.engine)
